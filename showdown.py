e80b03dd27e10844da55bc2d3fb35a6a66240690
itachi363 <itachi363@yahoo.com>
2022-03-07 13:53:54 -0500

    foundations to game

--- showdown.py
+++ showdown.py
@@ -16,3 +16,29 @@ class Showdown:
         spock = Spock
         ai = AI
         human = Human
+
+    def run_game(self):
+        self.display_welcome
+
+    def display_welcome(self):
+        print("Lets get started, ready your hands.")
+
+    def battle(self):
+        self.battle_ongoing = True
+        while self.battle_ongoing == True:
+            pass
+
+    def ai_turn(self, AI):
+        pass
+
+    def human_turn(self, human):
+        pass
+
+    def show_ai_options(self):
+        pass
+
+    def show_human_options(self):
+        pass
+
+    def display_winners(self):
+        pass
